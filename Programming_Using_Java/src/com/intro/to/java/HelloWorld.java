12a1bdf50d6f74bd6e3b84bd00b59499a14f96fa
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2021-02-11 17:55:16 -0500

    initial

---
+++ Programming_Using_Java/src/com/intro/to/java/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.intro.to.java;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java! You nut!");
+	} 
+}
---
+++ Programming_Using_Java/src/com/keywords/variables/identifiers/datatypes/Example1.java
@@ -0,0 +1,39 @@
+package com.keywords.variables.identifiers.datatypes;
+
+public class Example1 {
+	public static void main(String[] args) {
+		// 1. Keywords 
+		
+		/* In the above 'main' method declaration
+		 * public, static, and void are all keywords
+		 * keywords cannot be used as variable names in Java */
+		
+		// 2. Variables
+		
+		/* Variables are used to store data
+		 * Variables themselves indicate an allocation 
+		 * of memory in which a value is retained */
+		
+		// Variable Declaration
+		String name;
+		
+		// Variable initialization
+		name = "Nick G";
+		
+		// Variable declaration and initialization in one line
+		int age = 23;
+		
+		System.out.println(
+				"My name is " + name + 
+				" and I'm " + age + 
+				" and I love Java programming!"
+		);
+		
+		// 3. Identifiers
+		// In the above variable statements,
+		// age and name are identifiers
+		 
+		// 4. Data Types
+		
+	}
+}
